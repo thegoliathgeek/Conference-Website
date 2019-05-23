f9b15dba07dcbaa27da4d63f01e8eaed10f52361
Dhanush4444 <dhanushdazz@gmail.com>
2019-05-23 17:58:37 +0530

    Completed

--- js/dynamicrender.js
+++ js/dynamicrender.js
@@ -202,3 +202,408 @@ function advisoryCommitee() {
     `;
 
 }
+
+
+function technicalCommitee() {
+
+    document.getElementById('homebut').classList.remove('active');
+
+    document.getElementById('headerId').innerHTML = `
+     <div class="row" style="margin-top: 100px">
+    <div class="col-12">
+    <div class="text-center">
+    <h4 style="color: #630927;">Technical Advisory Committee</h4>
+</div>
+</div>
+</div>
+    `;
+
+    document.getElementById('dynamicrender').innerHTML = `
+    <div class="row">
+
+        <div class="col-5" style="margin-top: 30px; position:relative; z-index: -1000">
+        <h3 style="color: #630927">Name</h3>
+        <ul>
+        <li>Dr. K. Sreenivasa Rao</li>
+        <li>Dr. K. Srirama Murthy</li>
+        <li>Dr. Suryakanth  V Gangashetty</li>
+        <li>Dr. Dileep A. D.</li>
+        <li>Dr. Veena T.</li>
+        <li>Dr. Deepak K T</li>
+        <li>Dr. Narendra N. P.</li>
+        <li>Dr. Nayeemullah Khan</li>
+        <li>Dr. Guruprasad</li>
+        <li>Dr. Cyril Prasanna Raj</li>
+        <li>Dr. Anil Kumar Vuppala</li>
+        <li>Dr. Somashekhar Bhat</li>
+        <li>Dr. Neelam Sinha</li>
+        <li>Dr. P.G. Poonacha</li>
+        <li>Dr. Vaishali V. Patil</li>
+        <li>Manjunath</li>
+        
+        
+</ul>
+</div>
+
+    <div class="col-5" style="margin-top: 30px; position:relative; z-index: -1000">
+    <h3 style="color: #630927">Affliation</h3>
+        <ul>
+        <li>IIT Kharagpur</li>
+        <li>IIT Hyderabad</li>
+        <li>IIIT Hyderabad</li>
+        <li>IIT Mandi</li>
+        <li>NIT Goa</li>
+        <li>IIT Dharwad</li>
+        <li>Aalto University, Espoo, Finland</li>
+        <li>VIT, Chennai</li>
+        <li>Audience, Bangalore</li>
+        <li>MS Engineering College, Bangalore</li>
+        <li>IIIT, Hyderabad</li>
+        <li>MIT, Manipal</li>
+        <li>IIIT, Bangalore</li>
+        <li>IIIT, Bangalore</li>
+        <li>IIIT, Pune</li>
+        <li>ISRO, Banglore</li>
+        
+        
+</ul>
+</div>
+
+</div>
+    `;
+
+}
+
+function studentOutreach() {
+
+    document.getElementById('homebut').classList.remove('active');
+
+    document.getElementById('headerId').innerHTML = `
+     <div class="row" style="margin-top: 100px">
+    <div class="col-12">
+    <div class="text-center">
+    <h4 style="color: #630927;">Advisory Committee Member</h4>
+</div>
+</div>
+</div>
+    `;
+
+    document.getElementById('dynamicrender').innerHTML = `
+    <div class="row">
+
+        <div class="col-5" style="margin-top: 30px; position:relative; z-index: -1000">
+        <h3 style="color: #630927">Name</h3>
+        <ul>
+        <li>Indranil Sarkar</li>
+        <li>Deepak Dasalukunte</li>
+        <li>Bhanuprakash Venakatesh</li>
+        <li>Akhilesh</li>
+        <li>Vikas R Shivaprabhu</li>
+       
+        
+        
+</ul>
+</div>
+
+    <div class="col-5" style="margin-top: 30px; position:relative; z-index: -1000">
+    <h3 style="color: #630927">Affliation</h3>
+        <ul>
+        <li>Technology Specialist, Fish and Richardson, Boston, USA</li>
+        <li>Lantiq Communications, India Pvt. Ltd.</li>
+        <li>Honeywell Technology Solutions, Bangalore</li>
+        <li>Arizona State University, Tempe</li>
+        <li>Senior Software Engineer, Blackmagic Design, Colorado Springs, Colorado</li>
+
+        
+</ul>
+</div>
+
+</div>
+    `;
+
+
+}
+
+
+function program() {
+
+    document.getElementById('homebut').classList.remove('active');
+
+    document.getElementById('headerId').innerHTML = `
+     <div class="row" style="margin-top: 100px">
+    <div class="col-12">
+    <div class="text-center">
+    <h4 style="color: #630927;">Speakers</h4>
+</div>
+</div>
+</div>
+    `;
+
+    document.getElementById('dynamicrender').innerHTML = `
+    <div class="row">
+    <div class="col-md-16">
+    <div class="text-center">
+    <h1>To be updated soon</h1>
+</div>
+</div>
+</div>
+    `;
+}
+
+
+function callForPapers() {
+
+
+    document.getElementById('homebut').classList.remove('active');
+
+    document.getElementById('headerId').innerHTML = `
+     <div class="row" style="margin-top: 100px">
+    <div class="col-12">
+    <div class="text-center">
+    <h4 style="color: #630927;">Technical Advisory Committee</h4>
+</div>
+</div>
+</div>
+    `;
+
+    document.getElementById('dynamicrender').innerHTML = `
+    <div class="row" style="margin-top: 20px; position: relative; z-index: -1000">
+        <p style="color: #04267C; text-align: justify"><b>Prospective authors are invited to submit technical papers of 
+        their previously unpublished work. Detailed information about the electronic paper submission process can be 
+        found in the FOR PARTICIPANTS or PAPER SUBMISSION section. All presented papers will be submitted to the IEEE 
+        for possible inclusion in IEEEXplore. Topics of interest include, but are not limited to</b></p>
+    </div>
+    
+<div class="row" style="margin-top: 20px">
+
+<div class="col-4">
+<h5 style="color: #630927">Track 1</h5>
+<h4 style="color: #630927">Communication</h4>
+
+<ul style="font-size: 15px;">
+<li>Information Theory and Coding</li>
+<li>Wireless and Broadband Communication</li>
+<li>RF and Microwave Communication</li>
+<li>Satellite Communication</li>
+<li>Antenna Theory and Propagation</li>
+<li>Cognitive Radio</li>
+<li>Cooperative Communication</li>
+</ul>
+</div>
+<!--COl 1-->
+
+
+<div class="col-4">
+<h5 style="color: #630927">Track 2</h5>
+<h4 style="color: #630927">VLSI</h4>
+<ul style="font-size: 15px;">
+<li>Analog design and mixed signal IC Design</li>
+<li>Prototyping, Validation, Verification, Modeling and Simulation.</li>
+<li>System-on-Chip Design</li>
+<li>Deep Submicron Design and Modeling Issues, New Devices and MEMS</li>
+<li>CAD and Tools, Testability and Design for Test</li>
+</ul>
+</div>
+<style>
+li{
+color: #04267C;
+}
+</style>
+
+<!--Col 2-->
+<div class="col-4">
+<h5 style="color: #630927">Track 3</h5>
+<h4 style="color: #630927">Signal Proccesing</h4>
+<ul style="font-size: 15px">
+<li>Image and Video Processing</li>
+<li>Speech and Audio Processing</li>
+<li>Computer Vision</li>
+<li>Biomedical Signal Processing</li>
+<li>Compressive Sensing</li>
+<li>Design and Implementation of Signal Processing Systems</li>
+</ul>
+</div>
+</div>
+
+<!--Col 3-->
+
+`;
+
+}
+
+
+function submitPaper() {
+
+    document.getElementById('homebut').classList.remove('active');
+
+    document.getElementById('headerId').innerHTML = `
+     <div class="row" style="margin-top: 100px">
+    <div class="col-12">
+    <div class="text-center">
+    <h4 style="color: #630927;">Paper Submission</h4>
+</div>
+</div>
+</div>
+    `;
+
+    document.getElementById('dynamicrender').innerHTML = `
+    <div class="row" style="margin-top: 50px; position: relative; z-index: -1000">
+    <div class="col-md-12">
+    <div class="text-center">
+    <h2 style="color: #630927">Click GO to submit your paper</h2>
+</div>
+
+<div class="row" style="margin-top: 30px; position:relative;z-index: -1000">
+<div class="col-md-12">
+<div class="text-center">
+<a href="https://docs.google.com/forms/d/1R1Oh6L4ckbh_vJs2evM14UoKgMcmht9EGI7UJvd7EQo/edit" class="btn btn-outline-success">GO</a>
+</div>
+</div>
+</div>
+</div>
+    </div>
+    `;
+
+}
+
+
+function publication(){
+    document.getElementById('homebut').classList.remove('active');
+
+    document.getElementById('headerId').innerHTML = `
+     <div class="row" style="margin-top: 100px">
+    <div class="col-12">
+    <div class="text-center">
+    <h4 style="color: #630927;">Publication</h4>
+</div>
+</div>
+</div>
+    `;
+
+    document.getElementById('dynamicrender').innerHTML = `
+    <div class="row" style="margin-top: 50px; position: relative; z-index: -1000">
+    <div class="col-md-12">
+    <div class="text-center">
+    <h2 style="color: #630927">To be updated</h2>
+</div>
+</div>
+</div>
+
+    `;
+
+}
+
+function keyDates() {
+    document.getElementById('homebut').classList.remove('active');
+
+    document.getElementById('headerId').innerHTML = `
+<style>
+li{
+color: #04267C;
+font-size: 20px;
+}
+</style>
+     <div class="row" style="margin-top: 100px">
+    <div class="col-12">
+    <div class="text-center">
+    <h4 style="color: #630927;">Key Dates</h4>
+</div>
+</div>
+</div>
+    `;
+
+    document.getElementById('dynamicrender').innerHTML = `
+    <div class="row" style="margin-top: 50px; position: relative; z-index: -1000">
+    <div class="col-md-12">
+    <div class="text-center">
+    <ul style="text-align: justify">
+    <li>Submission Portal Opens: 1st September 2019</li>
+    <li>Full Paper Submission Deadline: 15th October 2019</li>
+    <li>Acceptance/Rejection Notification: 15th November 2019
+ Camera-Ready Paper due: 30th November 2019</li>
+ <li>Registration opens: 1st December 2019</li>
+</ul>
+</div>
+</div>
+</div>
+
+    `;
+}
+
+
+function accomodation() {
+    document.getElementById('homebut').classList.remove('active');
+
+    document.getElementById('headerId').innerHTML = `
+<style>
+li{
+color: #04267C;
+font-size: 20px;
+}
+</style>
+     <div class="row" style="margin-top: 100px">
+    <div class="col-12">
+    <div class="text-center">
+    <h4 style="color: #630927;">Accomodation</h4>
+</div>
+</div>
+</div>
+    `;
+
+    document.getElementById('dynamicrender').innerHTML = `
+    <div class="row" style="margin-top: 50px; position: relative; z-index: -1000">
+    <div class="col-md-12">
+    <div class="text-center">
+    <h2 style="color: #630927">To be updated soon</h2>
+</div>
+</div>
+</div>
+
+    `;
+}
+
+function contactUs() {
+
+    document.getElementById('homebut').classList.remove('active');
+
+    document.getElementById('headerId').innerHTML = `
+<style>
+li{
+color: #04267C;
+font-size: 20px;
+}
+</style>
+     <div class="row" style="margin-top: 100px">
+    <div class="col-12">
+    <div class="text-center">
+    <h4 style="color: #630927;">Contact Us</h4>
+</div>
+</div>
+</div>
+    `;
+
+    document.getElementById('dynamicrender').innerHTML = `
+<style>
+li{
+color: #04267C;
+font-size: 20px;
+list-style-type: none;
+}
+</style>
+    <div class="row" style="margin-top: 50px; position: relative; z-index: -1000">
+    <div class="col-md-12">
+    <!--<div class="text-center">-->
+    <h2 style="color: #630927">Reach Us</h2>
+    <ul style="text-align: justify">
+    <li>Dr. Veena Karjigi</li>
+    <li>Associate professor, Department of ECE</li>
+    <li>SIT, Tumkur</li>
+    <li>Ph- 9481489700</li>
+    <li>Mail ID- veena.karjigi@sit.ac.in</li>
+</ul>
+<!--</div>-->
+</div>
+</div>
+
+    `;
+}
